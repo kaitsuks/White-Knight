2a05179200709037a91b3d852b32f21b3eb1af46
kaitsuks <37402060+kaitsuks@users.noreply.github.com>
2018-11-18 18:24:50 +0200

    Weapon Choice
    Lisätty aseen valinta.

--- WhiteKnight2D/Assets/Scripts/ChangeWeapon.cs
+++ WhiteKnight2D/Assets/Scripts/ChangeWeapon.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class ChangeWeapon : MonoBehaviour
 {
     public GameObject spriteObjekti; // vaihda oman objektisi nimi! 
+    public GameObject oSword; // vaihda oman objektisi nimi! 
+    public GameObject oCrossbow; // vaihda oman objektisi nimi! 
+    public GameObject oJavelin; // vaihda oman objektisi nimi! 
     Dropdown dd;
     SpriteRenderer m_SpriteRenderer;
     Sprite sword;
@@ -13,33 +16,40 @@ public class ChangeWeapon : MonoBehaviour
     Sprite lasergun;
     Color color;
     int selected;
-    // Use this for initialization    
+    // Use this for initialization 
+    
     void Start()
     {
         dd = GetComponent<Dropdown>();
         m_SpriteRenderer = spriteObjekti.GetComponent<SpriteRenderer>(); // vaihda oman objektisi nimi!
-        sword = Resources.Load<UnityEngine.Sprite>("Characters/human");
-        javelin = Resources.Load<UnityEngine.Sprite>("Characters/human2");
-        lasergun = Resources.Load<UnityEngine.Sprite>("Characters/human3");
+        sword = Resources.Load<UnityEngine.Sprite>("Weapons/Sword");
+        lasergun = Resources.Load<UnityEngine.Sprite>("Weapons/Crossbow");
+        javelin = Resources.Load<UnityEngine.Sprite>("Weapons/Javelin");
+
+        //oSword = GameObject.Find("Sword");
+        //oCrossbow = GameObject.Find("Crossbow");
+        //oJavelin = GameObject.Find("Javelin");
     }
+
     // Update is called once per frame
     void Update()
     {
         //byte b = (byte)(sl.value * 255);
         //color = new Color32(128, 128, b, 255);
         //m_SpriteRenderer.color = color;
-        
-
-
-
     }
+
     public void Vaihda()
     {
         selected = dd.value;
         Debug.Log("Valittu " + selected);
+        oSword.SetActive(false);
+        oCrossbow.SetActive(false);
+        oJavelin.SetActive(false);
 
-        if (selected == 0) m_SpriteRenderer.sprite = sword;
-        if (selected == 1) m_SpriteRenderer.sprite = javelin;
-        if (selected == 2) m_SpriteRenderer.sprite = lasergun;
+        if (selected == 0) { m_SpriteRenderer.sprite = sword; oSword.SetActive(true); }
+        if (selected == 1) { m_SpriteRenderer.sprite = lasergun; oCrossbow.SetActive(true); }
+        if (selected == 2) { m_SpriteRenderer.sprite = javelin; oJavelin.SetActive(true); }
+        }
     }
-}
+
